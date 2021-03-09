16e21fdfd336cc3c261cde5df89726a62e4b288d
Anne-Marie Begin <Begin.Anne-Marie@ouranos.ca>
2021-03-09 14:43:07 -0500

    plot graphique nombre de simulation

--- _resolution_modeles.py
+++ _resolution_modeles.py
@@ -12,18 +12,19 @@ import xclim as xc
 from xclim import subset
 import xarray as xr
 import os
-import time
 import pandas as pd
-
-var = 'tasmax' #changer ligne 58
+import numpy as np
+import matplotlib.pyplot as plt
+var = 'pr' #changer ligne 58
 b_pt='brute'# brute ou posttraite
-SE='SE_1_CORDEX'#sous-ensemble
+SE='SE_1_complet'#sous-ensemble
+
 #open all files in repertory
 dd=[]
 path=('/tank/begin/weighting/'+SE+'/'+b_pt+'/'+var)  
 #path=('/tank/begin/weighting/SE_1/brute/'+var)  #observations
 files=os.listdir(path)
-files.reverse()
+files.sort()
 for i in files:
     dd.append(path+'/'+i)
     
@@ -32,10 +33,93 @@ ds=[]
 for j in range(0,len(files)):    
     ds.append(xr.open_mfdataset(dd[j]))
 
-mod=[]
-lat=[]
-for j in range(0,len(ds)):
-    mod.append({'modele':ds[j].model_id,'lat':ds[j].lat[0]-ds[j].lat[1],'lon':ds[j].lon[0]-ds[j].lon[1]})
-    lat.append(ds[j].lat[0]-ds[j].lat[1])
-df=pd.DataFrame.from_dict(mod)    
+#calcul distance en degre entre deux points de latitude
+#avec exception pour valeur manquante
+diff_lat=[]
+for i in range(0,len(ds)):
+    try:
+        diff_lat.append(float(np.mean(ds[i].lat[1]-ds[i].lat[0])))
+    except Exception as err:
+        diff_lat.append(None)
+        print(err)
+
+#calcul distance en degre entre deux points de longitude
+#avec exception grille NAM pas bonne valeur avec lon
+        # a partir de 110 lon en 2D alors [:,1]
+diff_lon=[]
+for i in range(0,110):
+    try:
+        diff_lon.append(float(np.mean(ds[i].rlon[1]-ds[i].rlon[0])))
+    except Exception as err:
+        diff_lon.append(float(np.mean(ds[i].lon[1]-ds[i].lon[0])))
+        print(err)
+for i in range(110,len(ds)):
+    try:
+        diff_lon.append(float(np.mean(ds[i].rlon[1]-ds[i].rlon[0])))
+    except Exception as err:
+        diff_lon.append(float(np.mean(ds[i].lon[:,1]-ds[i].lon[:,0])))
+        print(err)
+               
+#calcul de la distance en km des degres de latitude
+#avec exception pour valeur manquante        
+dis_lat=[]
+for i in range(0,len(diff_lat)):
+    try:
+        dis_lat.append(round(diff_lat[i]*2*3.1416*6371/360,0))
+    except Exception as err:
+        dis_lat.append(None)
+        print(err)   
+
+#calcul de la distance en km des degres de longitude
+#avec exception pour valeur manquante 
+dis_lon=[]
+for i in range(0,len(diff_lat)):
+    try:
+        dis_lon.append(round(diff_lon[i]*2*3.1416*6371/360,0))
+    except Exception as err:
+        dis_lon.append(None)
+        print(err)    
+
+#calcul distance moyenne entre lon et lat
+#avec exception pour valeur manquante 
+dis_moy=[]
+for i in range(0,len(diff_lat)):
+    try:
+        dis_moy.append(round((dis_lat[i]+dis_lon[i])/2,0))
+    except Exception as err:
+        dis_moy.append(dis_lon[i])
+        print(err)
+
+#condition 1 et 2*delta(X) plus petit que dimension bassin versant L1
+L1=[25,200,1000,5000,25000,50000,100000,200000,500000,1000000] #km2
+
+nombre_total_1=[]#1*deltaX
+for y in range(0,len(L1)):
+    nombre=[]
+    for i in range(0,len(ds)):
+        if dis_moy[i]<=np.sqrt(L1[y]):
+            nombre.append(i)
+    print(len(nombre))
+    nombre_total_1.append(len(nombre))
     
+nombre_total_2=[]#2*delataX
+for y in range(0,len(L1)):
+    nombre=[]
+    for i in range(0,len(ds)):
+        if 2*dis_moy[i]<=np.sqrt(L1[y]):
+            nombre.append(i)
+    print(len(nombre))
+    nombre_total_2.append(len(nombre))
+
+#plot dimension bassin versant vs nombre de simulation
+legende=['2*$\Delta$X','1*$\Delta$X']
+plt.figure(1)
+plt.plot(L1,nombre_total_2,'.r')
+plt.plot(L1,nombre_total_1,'xb')
+plt.legend(legende)
+plt.xscale('log')
+plt.grid(True,which="both", linestyle='-')
+plt.ylabel('Nombre de simulation sur 179')
+plt.xlabel('Taille du bassin versant km²')
+plt.title('Résolution minimale')
+plt.savefig('/tank/begin/weighting/plots/resolution_minimale')
